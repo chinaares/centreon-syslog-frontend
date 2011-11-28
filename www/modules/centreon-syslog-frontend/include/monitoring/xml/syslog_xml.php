a1d0d28696ae2eef195a5453ec48a112cea43f3c
Laurent Pinsivy <lpinsivy@merethis.com>
2011-11-28 09:28:22 +0000

    Bug #2816 : Displaying UTF8 in logs
    git-svn-id: http://svn.modules.centreon.com/centreon-syslog/trunk/centreon-syslog-frontend@514 7084d0f3-fa42-4235-85a3-747cc9c304f7

--- www/modules/centreon-syslog-frontend/include/monitoring/xml/syslog_xml.php
+++ www/modules/centreon-syslog-frontend/include/monitoring/xml/syslog_xml.php
@@ -58,18 +58,18 @@
 	/*
 	 * Get selected option in lists
 	 */
-	if (isset($_GET['collector_id']) && $_GET['collector_id'] != "" )
+	if (isset($_GET['collector_id']) && $_GET['collector_id'] != "")
 		$collector_id = $_GET['collector_id'];
 	else
 		$collector_id = "";
 		
 	
-	if (isset($_GET['Ffacility']) && $_GET['Ffacility'] != "")
+	if (isset($_GET['Ffacility']) && $_GET['Ffacility'] != "" && $_GET['Ffacility'] != "undefined")
 		$Ffacility_selected = $_GET['Ffacility'];
 	else
 		$Ffacility_selected = "";
 			
-	if (isset($_GET['Fseverity']) && $_GET['Fseverity'] != "")
+	if (isset($_GET['Fseverity']) && $_GET['Fseverity'] != "" && $_GET['Fseverity'] != "undefined")
 		$Fseverity_selected = $_GET['Fseverity'];
 	else
 		$Fseverity_selected = "";
@@ -79,7 +79,11 @@
 	    header('Content-Type: text/xml');
     	header('Pragma: no-cache');
     	header('Expires: 0');
-    	header('Cache-Control: no-cache, must-revalidate'); 
+    	header('Cache-Control: no-cache, must-revalidate');
+    	$buffer = new SyslogXML();
+    	$buffer->startElement("error");
+    	$buffer->writeElement("msg", _("Please select a collector."));
+    	$buffer->endElement();
     	$buffer->output();
     	exit;
 	}
@@ -88,15 +92,16 @@
 	 */
 	$pearDB = new syslogDB("centreon");
 	$pearDB_syslog = new SyslogDB("syslog", $collector_id);
+	$cfg_syslog = getSyslogOption($collector_id);
 	
 	$sql_filter = array();
-	if (isset($_GET['program']) && $_GET['program'] != "" )
+	if (isset($_GET['program']) && $_GET['program'] != "" && $_GET['program'] != "undefined")
 		array_push($sql_filter ," (program = '". htmlentities($_GET['program'] , ENT_QUOTES) ."')  ");
 
-	if (isset($_GET['host']) && $_GET['host'] != "")
+	if (isset($_GET['host']) && $_GET['host'] != "" && $_GET['host'] != "undefined")
 		array_push($sql_filter ," (host = '". htmlentities($_GET['host'] , ENT_QUOTES) ."')  ");	
 
-	if (isset($_GET['facility']) && $_GET['facility'] != "") {
+	if (isset($_GET['facility']) && $_GET['facility'] != "" && $_GET['facility'] != "undefined") {
 		if ((strcmp($Ffacility_selected, "") == 0) || (strcmp($Ffacility_selected, "eq") == 0)) {
 			array_push($sql_filter ," (facility = '". htmlentities($_GET['facility'] , ENT_QUOTES) ."')  ");
 		} else {
@@ -113,7 +118,7 @@
 		}
 	}
 
-	if (isset($_GET['severity']) && $_GET['severity'] != "")
+	if (isset($_GET['severity']) && $_GET['severity'] != "" && $_GET['severity'] != "undefined") {
 		if ((strcmp($Fseverity_selected, "") == 0) || (strcmp($Fseverity_selected, "eq") == 0)) {
 			array_push($sql_filter ," (priority = '". htmlentities($_GET['severity'] , ENT_QUOTES) ."')  ");
 		} else {
@@ -128,15 +133,16 @@
 			}
 			array_push($sql_filter ," (priority IN (".$list."))  ");
 		}
+	}
 
 	$req_sql_filter = "";	
 	if (isset( $sql_filter ))
 		$req_sql_filter = join(" AND " , $sql_filter);
 
 	if ($req_sql_filter != "")
-		$req = "SELECT * FROM logs WHERE " .  $req_sql_filter . " ORDER BY datetime DESC LIMIT 50";			
+		$req = "SELECT * FROM ".$cfg_syslog["db_table_logs"]." WHERE ".$req_sql_filter." ORDER BY datetime DESC LIMIT 50";			
 	else
-		$req = "SELECT * FROM logs ORDER BY datetime DESC LIMIT 50";	
+		$req = "SELECT * FROM ".$cfg_syslog["db_table_logs"]." ORDER BY datetime DESC LIMIT 50";	
 
 	$DBRESULT =& $pearDB_syslog->query($req);	
 
@@ -157,18 +163,24 @@
 		'critical' => 'sev_critical', 'crit' => 'sev_critical', 
 		'alert' => 'sev_alert', 'emerg' => 'sev_emerg');
 
- 	while($row =& $DBRESULT->fetchRow()) {
- 		$buffer->startElement("syslog");
- 		$buffer->writeElement("host", $row["host"]);
- 		$buffer->writeElement("facility", $row["facility"]);
- 		$buffer->writeElement("severity", $row["priority"]);
- 		$buffer->writeElement("datetime", $row["datetime"]);
- 		$buffer->writeElement("program", $row["program"]);
- 		$buffer->writeElement("msg", $row["msg"]);
- 		$style == "list_two" ? $style = "list_one" : $style = "list_two";
- 		$buffer->writeElement("style", $style); 		
- 		$buffer->writeElement("prio_class", $priority_color[$row['priority']]);
+ 	if ($DBRESULT->numRows() == 0) {
+ 		$buffer->startElement("error");
+ 		$buffer->writeElement("msg", _("No informations"));
  		$buffer->endElement();
+ 	} else {
+ 		while($row =& $DBRESULT->fetchRow()) {
+	 		$buffer->startElement("syslog");
+	 		$buffer->writeElement("host", $row["host"]);
+	 		$buffer->writeElement("facility", $row["facility"]);
+	 		$buffer->writeElement("severity", $row["priority"]);
+	 		$buffer->writeElement("datetime", $row["datetime"]);
+	 		$buffer->writeElement("program", $row["program"]);
+	 		$buffer->writeElement("msg", utf8_decode($row["msg"]));
+	 		$style == "list_two" ? $style = "list_one" : $style = "list_two";
+	 		$buffer->writeElement("style", $style); 		
+	 		$buffer->writeElement("prio_class", $priority_color[$row['priority']]);
+	 		$buffer->endElement();
+ 		}
  	}
 
  	$buffer->endElement();
