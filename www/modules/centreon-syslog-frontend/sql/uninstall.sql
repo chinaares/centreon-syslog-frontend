d4bd70e0c5663ab9cc7a4d3e527dbf64a281d3d5
Laurent Pinsivy <lpinsivy@merethis.com>
2011-12-29 10:54:01 +0000

    correct uninstall SQL requests for topology
    git-svn-id: http://svn.modules.centreon.com/centreon-syslog/trunk/centreon-syslog-frontend@537 7084d0f3-fa42-4235-85a3-747cc9c304f7

--- www/modules/centreon-syslog-frontend/sql/uninstall.sql
+++ www/modules/centreon-syslog-frontend/sql/uninstall.sql
@@ -7,11 +7,13 @@ DELETE FROM `topology` WHERE `topology_parent` = '204' AND `topology_name` = "Sy
 DELETE FROM `topology` WHERE `topology_page` = '20401' AND `topology_name` = "Monitoring";
 DELETE FROM `topology` WHERE `topology_page` = '20402' AND `topology_name` = "Search";
 DELETE FROM `topology` WHERE `topology_page` = '20403' AND `topology_name` = "Details";
+DELETE FROM `topology` WHERE `topology_page` = '204' AND `topology_name` = "Syslog";
 DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "Syslog";
 DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "Collectors";
 DELETE FROM `topology` WHERE `topology_page` = '60502' AND `topology_name` = "Collectors";
 DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "General";
 DELETE FROM `topology` WHERE `topology_page` = '60503' AND `topology_name` = "Resfresh";
+DELETE FROM `topology` WHERE `topology_page` = '605' AND `topology_name` = "Syslog";
 
 DELETE FROM `topology_JS` WHERE `id_page` = '204' LIMIT 1;
 DELETE FROM `topology_JS` WHERE `id_page` = '20401' LIMIT 1;
